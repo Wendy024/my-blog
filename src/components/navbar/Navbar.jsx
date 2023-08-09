8ce2a0237dd8824fbd8d2768c4fd9f02a7ce113c
Esther Nwanagu <chi44christ@gmail.com>
2023-08-09 10:30:40 +0100

    api commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { Context } from "../../pages/context/Context";
 
 export default function Navbar() {
   const {user, dispatch} = useContext(Context);
-  const PF = "http://localhost:5000/images";
+  const PF = "http://localhost:5000/images/";
 
   const handleLogout = () =>{
     dispatch({type: "LOGOUT"});
